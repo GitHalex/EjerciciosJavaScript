24d0cab18d601f6efddd6ad4b755edcc57c4c1c1
GitHalex <calex1731@gmail.com>
2023-03-30 00:06:39 -0400

    Ejercicio terminado de variables

--- js/variables.js
+++ js/variables.js
@@ -13,3 +13,11 @@ console.log(`La cadena tiene ${caracteres_texto.length} caracteres`);
 const cadena_sin_espacio = caracteres_texto.join("");
 console.log(cadena_sin_espacio);
 console.log(`La cadena mide sin espacios ${cadena_sin_espacio.length}`);
+
+const dato_year = 1958;
+let madona_year = 2023 - 1958;
+console.log(`Madona tiene ${madona_year} años`);
+
+let today = new Date();
+const year_today = today.getFullYear();
+console.log(`año de hoy ${year_today}`);
