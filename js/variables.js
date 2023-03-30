0188c5a3d2ec18c32cb85b2aada494d95c472262
GitHalex <calex1731@gmail.com>
2023-03-29 23:38:26 -0400

    Agregando los archivos principales

---
+++ js/variables.js
@@ -0,0 +1,21 @@
+const texto =
+  "Compartir el conocimiento es el acto más fundamental de la amistad. Porque es una forma de dar algo sin perder nada.";
+const tam = texto.length;
+console.log(texto.toUpperCase());
+console.log(texto.toLowerCase());
+
+console.log(tam);
+
+const cadenaNormal =
+  "El 90% de nuestras decisiones las toma nuestro subconsciente";
+
+// Convierte en mayusculas
+const cadenaMayusculas = cadenaNormal.toUpperCase();
+
+// Convierte en minúsculas
+const cadenaMinusculas = cadenaNormal.toLowerCase();
+
+// Número de carácteres
+const longitud = texto.length;
+
+console.log(cadenaMayusculas, cadenaMinusculas, longitud);
